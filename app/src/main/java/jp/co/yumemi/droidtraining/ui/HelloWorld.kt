8e38a374336afdcbc002bc1219709a6734f6dd21
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-11-26 07:55:52 +0900

    Composeの導入 #9
    HelloWorldの表示完了

---
+++ app/src/main/java/jp/co/yumemi/droidtraining/ui/HelloWorld.kt
@@ -0,0 +1,16 @@
+package jp.co.yumemi.droidtraining.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HelloWorld() {
+    Text("HelloWorld")
+}
+
+@Preview
+@Composable
+fun HelloWorldPreview() {
+    HelloWorld()
+}
