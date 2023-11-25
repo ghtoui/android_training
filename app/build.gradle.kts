15e29e5aa52d203f0a8677a1d4e73b2f9cb831cf
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-11-25 19:34:12 +0900

    ライブラリとSDKの更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,13 @@ plugins {
 }
 android {
     namespace = "jp.co.yumemi.droidtraining"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "jp.co.yumemi.droidtraining"
         minSdk = 27
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,9 +38,9 @@ android {
 dependencies {
     implementation(project(":api"))
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
