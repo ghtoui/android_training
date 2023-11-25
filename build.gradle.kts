51a7661fee36f376700bca0e4e1c93aec7d3fafe
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-11-25 19:03:42 +0900

    gradleをkotlinで書き換えた

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.9.10"
+    val kotlin_version = "1.9.10"
     repositories {
         google()
         mavenCentral()
-        maven {
-            url "https://plugins.gradle.org/m2/"
-        }
+        maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.4.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath "org.jlleitschuh.gradle:ktlint-gradle:11.1.0"
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.1.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -25,20 +25,20 @@ allprojects {
     }
 }
 
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
 
 subprojects {
     // Gradle Plugin for ktlint
-    apply plugin: "org.jlleitschuh.gradle.ktlint"
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
-    ktlint {
-        android = true
-        ignoreFailures = true
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        ignoreFailures.set(true)
         reporters {
-            reporter "plain"
-            reporter "checkstyle"
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.CHECKSTYLE)
         }
         filter {
             include("src/**/*.kt")
