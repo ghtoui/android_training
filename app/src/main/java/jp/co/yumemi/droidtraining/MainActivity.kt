96d0c5a483de4f8f3f884cdf302b47155515a937
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-11-29 17:51:37 +0900

    viewModelにhiltを使ってDIと状態の保存
    SavedStateHandleで状態の保持方法を理解。保存するものは、Parcelableにすることが望ましいことも理解

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -1,12 +1,20 @@
 package jp.co.yumemi.droidtraining
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.droidtraining.ui.HomeScreen
+import jp.co.yumemi.droidtraining.ui.theme.AppTheme
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AppTheme {
+                HomeScreen()
+            }
+        }
     }
 }
--- app/src/main/java/jp/co/yumemi/droidtraining/model/WeatherState.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/model/WeatherState.kt
@@ -1,6 +1,9 @@
 package jp.co.yumemi.droidtraining.model
 
+import java.io.Serializable
+
+// savedStateHandleにそのまま保存するには, Serializable or Parcelize にする必要がある
 data class WeatherState(
-    val weatherSuccess: String?,
-    val showErrorDialog: Boolean
-)
+    val weatherSuccess: String? = null,
+    var showErrorDialog: Boolean = false
+) : Serializable
